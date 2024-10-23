b75fd4a2ace2352b9079ac56d3eaad34ccd477ec
AmoxV2 <60665323+AmoxV2@users.noreply.github.com>
2024-10-23 20:47:44 +0200

    Add Overriding function from cycle of life

--- QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -14,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("Lifecycle", "onCreate called")
 
         val questionTextView: TextView = findViewById(R.id.question_Texview)
         val buttonTrue: Button = findViewById(R.id.button_true)
@@ -70,8 +73,37 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Zła odpowiedź!", Toast.LENGTH_SHORT).show()
         }
     }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+
     private fun displayResult() {
         val resultMessage = "Koniec Quizu!!Twój wynik: $correctAnswersCount/${questionList.size} poprawnych odpowiedzi"
         Toast.makeText(this, resultMessage, Toast.LENGTH_LONG).show()
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d("Lifecycle", "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Lifecycle", "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Lifecycle", "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Lifecycle", "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Lifecycle", "onDestroy called")
     }
 }
