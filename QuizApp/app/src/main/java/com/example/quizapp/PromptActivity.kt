e6bed5fe2fe0352aa93733596c109f8732ded7e5
AmoxV2 <60665323+AmoxV2@users.noreply.github.com>
2024-10-23 22:50:25 +0200

    Showing Question

--- QuizApp/app/src/main/java/com/example/quizapp/PromptActivity.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/PromptActivity.kt
@@ -1,25 +1,40 @@
 package com.example.quizapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.quizapp.MainActivity.Companion.KEY_CURRENT_INDEX
 
 class PromptActivity : AppCompatActivity() {
 
+    private lateinit var showCorrectAnswerButton: Button
+    private lateinit var answerTextView: TextView
+    private var correctAnswer: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_prompt)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val correctAnswer = intent.getBooleanExtra("correctAnswer", false)
-    }
 
+
+        correctAnswer = intent.getBooleanExtra("correctAnswer", false)
+        showCorrectAnswerButton = findViewById(R.id.show_question)
+        answerTextView = findViewById(R.id.question_answer)
+
+
+        showCorrectAnswerButton.setOnClickListener {
+            val answer = if (correctAnswer) R.string.button_true else R.string.button_false
+            answerTextView.setText(answer)
+        }
+    }
 }
